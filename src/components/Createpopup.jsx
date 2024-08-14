8145c3446053622ae5ed4f61ec23608c40364f08
+ <+>
2024-08-14 10:23:21 +0300

    imported the popup component to app component

--- src/components/Createpopup.jsx
+++ src/components/Createpopup.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 
-function Createpopup() {
+function Createpopup({isOpen, onClose}) {
   return (
     <div className=''>
-      
+      <Popup open={isOpen} onClose={onClose} modal>
+            <div className="bg-gray-200 rounded-lg shadow-xl p-6" style={{ width: '600px' }}>
+                <h2 className="text-2xl font-bold text-center text-gray-700 mb-4">Create popup</h2>
+            </div>
+        </Popup>
     </div>
   )
 }
