e2e07f303877d645465672629a9a70c02e66e050
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-08-14 14:16:17 +0300

    updated the projectpage component

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,16 +1,46 @@
 import React from 'react'
 
+
 const ProjectPage = () => {
+
   return (
-    <div>
-        <nav>
-            <a href="https://i.pinimg.com/564x/87/3b/8e/873b8eec2f539668393e2c68cd0b2bc5.jpg"></a>
-          <ul >
-            <li><Link to="/" >Home</Link></li>
-            <li><Link to="/about" >About</Link></li>
-            <li><Link to="/logout" >Logout</Link></li>
+    <div className=''>
+        <nav className=' flex items-center justify-around'>
+           <img className='w-16 h-16' src="https://i.pinimg.com/564x/87/3b/8e/873b8eec2f539668393e2c68cd0b2bc5.jpg" alt="" /> 
+           
+          <ul className='flex gap-8 items-center' >
+          
+            <li>Home</li>
+            <li> About</li>
+            <button className=' border-2 p-2 m-4 w-60 rounded-md' >search</button>
+            <li>Logout</li>
           </ul>
+   
         </nav>
+        <div className=' flex justify-evenly mt-14 p-16'>
+        <div className="text-lg font-semibold ">User’s name</div>
+        <button className=" border-2 px-4 py-2 rounded ">
+              Create Project
+            </button>
+            </div>
+
+      <main className="text-center ">
+        <h1 className="text-2xl font-bold mb-4">Welcome to Project Management </h1>
+    
+        
+        <div className="mb-4">
+          
+            
+          
+        </div>
+        
+        <p className="text-gray-700">+ Click here to create a new project</p>
+      </main>
+
+      <footer className=' justify-center flex flex-end' >
+        <p>Contact us: <a href="projectmanagement@gmail.com" className="text-blue-500 hover:underline">projectmanagement@gmail.com</a></p>
+      </footer>
+
     </div>
   )
 }
