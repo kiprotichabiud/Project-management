8c507719dfc16be11d89d7d52622edfa5ee94b44
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-08-14 09:49:13 +0300

    updated  the projectpage component

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const ProjectPage = () => {
   return (
-    <div>ProjectPage</div>
+    <div>
+        <nav>
+          <ul >
+            <li><Link to="/" >Home</Link></li>
+            <li><Link to="/about" >About</Link></li>
+            <li><Link to="/logout" >Logout</Link></li>
+          </ul>
+        </nav>
+    </div>
   )
 }
 
