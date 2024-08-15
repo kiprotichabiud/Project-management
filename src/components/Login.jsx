bfaca702a610fb34fe78202a9eaba53a40472d43
Nelly <nmatu308@gmail.com>
2024-08-15 09:29:42 +0300

    Created login box in login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+<div  className='form-b0x-login'> 
+   <form action="">
+    <h1>Login form </h1>
+    <div className='input-box'> 
+     <input type="text" placeholder='Name'  required/>    
+    </div>
+    <div className='input-box'>
+        <input type="text" placeholder='Username' required/>
+    </div>
+    
+  
+
+   </form>
+</div>
+      
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+
+// function SignUp() {
+//   return (
+//     <div>
+//       <div  className='form-b0x-signup'> 
+
+//    <form action="">
+//     <h1>Signup form </h1>
+//     <div className='input-box'> 
+//      <input type="text" placeholder='Name'  required/>    
+//     </div>
+//     <div className='input-box'>
+//         <input type="text" placeholder='Username' required/>
+//     </div>
+
+//     </div>
+//     </form>
+// </div>
+      
+    
+//   )
+// }
+
+// export default SignUp
