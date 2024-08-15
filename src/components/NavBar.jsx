9ab8163b33978944247147020ea7023cf906e26b
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-08-15 13:41:28 +0300

    added stylings to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
-    <nav className="navbar">
+    <nav className=" flex justify-between p-4 m-2 shadow-md  ">
       <img src="https://i.pinimg.com/564x/87/3b/8e/873b8eec2f539668393e2c68cd0b2bc5.jpg" alt="project logo" className='w-16 h-16' />
       <ul className="flex gap-8 items-center">
         <li><Link to="/">Home</Link></li>
